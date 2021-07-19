178be936664b206c1a7be09a59be0d49c7fb68b9
sookiemonster <dsooknanan20@stuy.edu>
2021-07-19 19:12:35 -0400

    Fix verification bug

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
   $("#choose-file").change(function() {
     var value = $("#choose-file").val();
     if (!value.endsWith(".zip")) {
-      alert("Please select a zip file.");
-      $("#choose-file").val('');
+      if (value == "" || value == undefined) {
+        alert("Please select a file to upload.");
+      } else {
+        alert("Please select a zip file.");
+        $("#choose-file").val('');
+      }
       return false;
-    }
-    else if (value != "" && value != undefined) {
+    } else if (value != "" && value != undefined) {
       $("#choose-text").html(
         "Selected File:"
       )
