511f345491f15b841c9b7907740a25833e5012db
sookiemonster <dsooknanan20@stuy.edu>
2021-07-22 11:34:44 -0400

    Update styling & top artists

--- app/static/js/output.js
+++ app/static/js/output.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
 
   $("#artist-select").click(function() {
-    $(".song-title, .song-artist").hide();
-    $(".artist").show();
+    $(".song-title, .song-artist, .song-icon").hide();
+    $(".artist, .artist-icon").show();
     $("#popular-header").html("Your Favorite Artists");
+    $("#artist-select").addClass("active-button");
+    $("#song-select").removeClass("active-button");
   });
 
   $("#song-select").click(function() {
-    $(".artist").hide();
-    $(".song-title, .song-artist").show();
+    $(".artist, .artist-icon").hide();
+    $(".song-title, .song-artist, .song-icon").show();
     $("#popular-header").html("Your Favorite Songs");
+    $("#song-select").addClass("active-button");
+    $("#artist-select").removeClass("active-button");
   });
 });
