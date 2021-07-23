731bfd760ab136d1cc2fd00bcc168d782b91f93b
sookiemonster <dsooknanan20@stuy.edu>
2021-07-23 17:51:08 -0400

    Add Top 10 Favorites

--- app/static/js/output.js
+++ app/static/js/output.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
 
+  // var top_expanded = false;
+
   $(".artist, .artist-img, .artist-count").hide();
 
   $("#icon-image").filter(function(index) {
     return $(this).attr('src')==='';
   }).hide();
 
-  $("#artist-select").click(function () {
+  $("#artist-select").click(function() {
     $(".song-title, .song-artist, .track-img, .track-count").hide();
     $(".artist, .artist-img, .artist-count").show();
     $("#popular-header").html("Your Favorite Artists");
@@ -14,11 +16,20 @@ $(document).ready(function () {
     $("#song-select").removeClass("active-button");
   });
 
-  $("#song-select").click(function () {
+  $("#song-select").click(function() {
     $(".artist, .artist-img, .artist-count").hide();
     $(".song-title, .song-artist, .track-img, .track-count").show();
     $("#popular-header").html("Your Favorite Songs");
     $("#song-select").addClass("active-button");
     $("#artist-select").removeClass("active-button");
+  });
+
+  $("#expand-button").click(function() {
+    $(".top-list li:nth-of-type(1n + 6)").toggle();
+    if ($("#expand-arrow").hasClass("flip")) {
+      $("#expand-arrow").removeClass("flip");
+    } else {
+      $("#expand-arrow").addClass("flip");
+    }
   });
 });
