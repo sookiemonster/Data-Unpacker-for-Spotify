b48e15d4f82450ed3ba85ba9ad9713ec9392e70e
sookiemonster <dsooknanan20@stuy.edu>
2021-07-24 21:38:21 -0400

    Remove listening frequency if screen's too big

--- app/static/js/output.js
+++ app/static/js/output.js
@@ -13,19 +13,40 @@ $(document).ready(function () {
   }).hide();
 
   $("#artist-select").click(function() {
-    $(".song-title, .song-artist, .track-img, .track-count").hide();
-    $(".artist, .artist-img, .artist-count").show();
+    if($(window).width() > 500){
+      $(".artist-count").show();
+    }  
+
+    $(".song-title, .song-artist, .track-img, .track-count, .clip-container").hide();
+    $(".artist, .artist-img").show();
     $("#popular-header").html("Your Favorite Artists");
     $("#artist-select").addClass("active-button");
     $("#song-select").removeClass("active-button");
   });
 
   $("#song-select").click(function() {
-    $(".artist, .artist-img, .artist-count").hide();
-    $(".song-title, .song-artist, .track-img, .track-count").show();
+    if($(window).width() > 500){
+      $(".track-count").show();
+    }
+
+    $(".artist, .artist-count, .artist-img").hide();
+    $(".song-title, .song-artist, .track-img, .clip-container").show();
     $("#popular-header").html("Your Favorite Songs");
     $("#song-select").addClass("active-button");
     $("#artist-select").removeClass("active-button");
   });
-  
+
+  $(window).on("resize",function() {  
+    if($(window).width() <= 500) {
+      $(".track-count").hide();
+      $(".artist-count").hide();
+    } else {
+      if ($("#song-select").hasClass("active-button")) {
+        $(".track-count").show();
+      } else {
+        $(".artist-count").show();
+      }
+    }
+ });
+
 });
