7824b00d0352a3afa8e0f5788cd1596da4087011
shushu <golpos369@gmail.com>
2021-08-04 15:11:47 -0400

    should deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,28 @@
+from dotenv import load_dotenv
 from flask import Flask
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
+from os import getenv
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
 from wtforms import SubmitField
 from wtforms.validators import DataRequired
 
+# Load environment variables
+load_dotenv()
+
 app = Flask(__name__)
-# change this to something more secure later lmao
-app.config["SECRET_KEY"] = "fZYidiMjTU@Qg&J*@QM6mnpNjEcXChqP@KjCkz!5fcyMarb4H*57yGRWYP@U4Pn#"
+app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 app.config["MAX_CONTENT_LENGTH"] = 2 * 1024 * 1024 # makes sure file is not larger than 2MB
+
 class ZipUploadForm(FlaskForm):
     file = FileField('file', validators=[DataRequired()])
     submit = SubmitField('submit')
 
 # credentials for spotify dev acc (make this more secure later)
 spotify = Spotify(auth_manager=SpotifyClientCredentials(
-    client_id="5d0448251c3e422eb0a97bf0852db996",
-    client_secret="16f9c54d622c422fb3523e3b06b635d6"
+    client_id=getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=getenv("SPOTIFY_CLIENT_SECRET")
 ))
 
 from app import views
